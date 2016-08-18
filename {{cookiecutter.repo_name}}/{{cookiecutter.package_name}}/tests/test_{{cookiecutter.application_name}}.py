1f61d400d7e12ad7283aecb1cb89b14c419e8e19
mandeepbhutani <bhutanimandeep@gmail.com>
2016-08-18 00:54:52 -0500

    Added pytest-mock plugin to setup.py and added more tests to test suite

--- {{cookiecutter.repo_name}}/{{cookiecutter.package_name}}/tests/test_{{cookiecutter.application_name}}.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.package_name}}/tests/test_{{cookiecutter.application_name}}.py
@@ -20,7 +20,29 @@ class Test{{ cookiecutter.application_title }}:
 
     def setup(self):
         self.window = {{cookiecutter.application_name }}.{{cookiecutter.application_title }}()
+        {% if cookiecutter.insert_menubar == 'yes' %}
+        self.menu_bar = {{cookiecutter.application_name}}.MenuBar()
+        {% endif %}
         self.window.show()
 
     def test_window_title(self):
         assert self.window.windowTitle() == '{{ cookiecutter.application_title }}'
+
+    def test_window_geometry(self):
+        assert self.window.width() == 1024
+        assert self.window.height() == 768
+
+    {% if cookiecutter.insert_menubar == 'yes' %}
+    def test_open_file(self, qtbot, mock):
+        qtbot.mouseClick(self.menu_bar.file_sub_menu, Qt.LeftButton)
+        qtbot.keyClick(self.menu_bar.file_sub_menu, Qt.Key_Down)
+        mock.patch.object(QFileDialog, 'getOpenFileName', return_value=('README.rst', '*.rst'))
+        qtbot.keyClick(self.menu_bar.file_sub_menu, Qt.Key_Enter)
+
+    def test_about_dialog(self, qtbot, mock):
+        qtbot.mouseClick(self.menu_bar.help_sub_menu, Qt.LeftButton)
+        qtbot.keyClick(self.menu_bar.help_sub_menu, Qt.Key_Down)
+        mock.patch.object(QDialog, 'exec_', return_value='finished')
+        qtbot.keyClick(self.menu_bar.help_sub_menu, Qt.Key_Enter)
+    {% endif %}
+
