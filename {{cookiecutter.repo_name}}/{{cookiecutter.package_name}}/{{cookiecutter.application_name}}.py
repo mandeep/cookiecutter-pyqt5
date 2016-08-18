414d7a47dabddd79f716170825ec13f08022d0b1
mandeepbhutani <bhutanimandeep@gmail.com>
2016-08-18 14:16:57 -0500

    Added docstrings to classes and methods

--- {{cookiecutter.repo_name}}/{{cookiecutter.package_name}}/{{cookiecutter.application_name}}.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.package_name}}/{{cookiecutter.application_name}}.py
@@ -6,8 +6,12 @@ from PyQt5.QtWidgets import (QAction, QApplication, QDialog, QDesktopWidget, QFi
 
 {% if cookiecutter.insert_menubar == 'yes' %}
 class MenuBar(QMenuBar):
+    """MenuBar class creates a menu bar with necessary items to be used inside of a QMainWindow."""
 
     def __init__(self, parent=None):
+        """Instantiates the file dialogs and dialog boxes that are used to open files and show
+        messages. Also creates the file menu and help menu."""
+        
         super(MenuBar, self).__init__(parent)
 
         self.open_items = OpenDialogs()
@@ -17,6 +21,9 @@ class MenuBar(QMenuBar):
         self.help_menu()
 
     def file_menu(self):
+        """Creates a file menu for the menu bar with an Open File item that opens a
+        file dialog."""
+
         self.file_sub_menu = self.addMenu('File')
 
         self.open_action = QAction('Open File', self)
@@ -27,6 +34,8 @@ class MenuBar(QMenuBar):
         self.file_sub_menu.addAction(self.open_action)
 
     def help_menu(self):
+        """Creates a help menu for the menu bar with a dialog box used to show users helpful
+        information about the application."""
         self.help_sub_menu = self.addMenu('Help')
 
         self.about_action = QAction('About', self)
@@ -38,11 +47,14 @@ class MenuBar(QMenuBar):
 
 
 class OpenDialogs(QFileDialog):
+    """The OpenDialogs class stores all activities related to opening files with file dialogs."""
 
     def __init__(self, parent=None):
         super(OpenDialogs, self).__init__(parent)
 
     def open_file(self):
+        """Opens a QFileDialog to allow the user to open a file into the application. The template
+        creates the dialog and simply reads it with the context manager."""
 
         filename, accepted = QFileDialog.getOpenFileName(self, 'Open File')
 
@@ -52,11 +64,15 @@ class OpenDialogs(QFileDialog):
 
 
 class Dialogs(QDialog):
+    """Because of the wide array of uses of QDialogs, Dialogs is created to store any kind of
+    QDialog item that the user can create."""
 
     def __init__(self, parent=None):
         super(Dialogs, self).__init__(parent)
 
     def about_dialog(self):
+        """Creates a QDialog that shows the user information regarding the application. Uses layouts
+        to arrange the widgets on the dialog."""
 
         group_box = QGroupBox()
         layout = QHBoxLayout()
@@ -77,6 +93,8 @@ class Dialogs(QDialog):
 {% endif %}
 {% if cookiecutter.insert_statusbar == 'yes' %}
 class StatusBar(QToolBar):
+    """Creates a status bar to be added to the main window. Inherits QToolBar so that the user
+    may move the status bar wherever they please."""
 
     def __init__(self, parent=None):
         super(StatusBar, self).__init__(parent)
@@ -86,8 +104,12 @@ class StatusBar(QToolBar):
         self.addWidget(self.status_bar)
 {% endif %}
 class {{ cookiecutter.application_title }}(QMainWindow):
+    """Creates the main window that stores all of the widgets necessary for the application."""
 
     def __init__(self, parent=None):
+        """Initializes the window size and title and instantiates the menu bar and status bar
+        if selected by the user."""
+
         super({{ cookiecutter.application_title }}, self).__init__(parent)
         self.resize(1024, 768)
         self.setWindowTitle('{{ cookiecutter.application_title }}')
