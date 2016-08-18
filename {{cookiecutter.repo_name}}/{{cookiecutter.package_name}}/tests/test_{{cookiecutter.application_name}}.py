86e6931f28e66c1996d803e41a974d88be13f7d6
mandeepbhutani <bhutanimandeep@gmail.com>
2016-08-18 04:58:21 -0500

    Added pytest.fixtures and qtbot.add_widget

--- {{cookiecutter.repo_name}}/{{cookiecutter.package_name}}/tests/test_{{cookiecutter.application_name}}.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.package_name}}/tests/test_{{cookiecutter.application_name}}.py
@@ -8,42 +8,52 @@ test_{{ cookiecutter.package_name }}
 Tests for `{{ cookiecutter.package_name }}` module.
 """
 import sys
+import pytest
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import (QAction, QApplication, QDialog, QDesktopWidget, QFileDialog,
                              QGroupBox, QHBoxLayout, QLabel, QMainWindow, QMenuBar, QStatusBar,
                              QToolBar)
 from {{ cookiecutter.package_name }} import {{ cookiecutter.application_name }}
 
-app = QApplication(sys.argv)
+
+@pytest.fixture
+def window(qtbot):
+    new_window = {{cookiecutter.application_name }}.{{cookiecutter.application_title }}()
+    qtbot.add_widget(new_window)
+    new_window.show()
+    return new_window
 
 
-class Test{{ cookiecutter.application_title }}:
+{% if cookiecutter.insert_menubar == 'yes' %}
+@pytest.fixture
+def menu(qtbot):
+    new_menu_bar = {{cookiecutter.application_name}}.MenuBar()
+    qtbot.add_widget(new_menu_bar)
+    return new_menu_bar
+{% endif %}
 
-    def setup(self):
-        self.window = {{cookiecutter.application_name }}.{{cookiecutter.application_title }}()
-        {% if cookiecutter.insert_menubar == 'yes' %}
-        self.menu_bar = {{cookiecutter.application_name}}.MenuBar()
-        {% endif %}
-        self.window.show()
 
-    def test_window_title(self):
-        assert self.window.windowTitle() == '{{ cookiecutter.application_title }}'
+def test_window_title(window):
+    assert window.windowTitle() == '{{ cookiecutter.application_title }}'
 
-    def test_window_geometry(self):
-        assert self.window.width() == 1024
-        assert self.window.height() == 768
 
-    {% if cookiecutter.insert_menubar == 'yes' %}
-    def test_open_file(self, qtbot, mock):
-        qtbot.mouseClick(self.menu_bar.file_sub_menu, Qt.LeftButton)
-        qtbot.keyClick(self.menu_bar.file_sub_menu, Qt.Key_Down)
-        mock.patch.object(QFileDialog, 'getOpenFileName', return_value=('README.rst', '*.rst'))
-        qtbot.keyClick(self.menu_bar.file_sub_menu, Qt.Key_Enter)
+def test_window_geometry(window):
+    assert window.width() == 1024
+    assert window.height() == 768
 
-    def test_about_dialog(self, qtbot, mock):
-        qtbot.mouseClick(self.menu_bar.help_sub_menu, Qt.LeftButton)
-        qtbot.keyClick(self.menu_bar.help_sub_menu, Qt.Key_Down)
-        mock.patch.object(QDialog, 'exec_', return_value='finished')
-        qtbot.keyClick(self.menu_bar.help_sub_menu, Qt.Key_Enter)
-    {% endif %}
+
+{% if cookiecutter.insert_menubar == 'yes' %}
+def test_open_file(window, menu, qtbot, mock):
+    qtbot.mouseClick(menu.file_sub_menu, Qt.LeftButton)
+    qtbot.keyClick(menu.file_sub_menu, Qt.Key_Down)
+    mock.patch.object(QFileDialog, 'getOpenFileName', return_value=('README.rst', '*.rst'))
+    qtbot.keyClick(menu.file_sub_menu, Qt.Key_Enter)
+
+
+def test_about_dialog(self, qtbot, mock):
+    qtbot.mouseClick(menu.help_sub_menu, Qt.LeftButton)
+    qtbot.keyClick(menu.help_sub_menu, Qt.Key_Down)
+    mock.patch.object(QDialog, 'exec_', return_value='finished')
+    qtbot.keyClick(menu.help_sub_menu, Qt.Key_Enter)
+{% endif %}
 
