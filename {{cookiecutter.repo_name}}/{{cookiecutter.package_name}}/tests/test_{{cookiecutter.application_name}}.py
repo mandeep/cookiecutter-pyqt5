7a537600b0721c52d513d82ac798bca7b0bcc99c
mandeepbhutani <bhutanimandeep@gmail.com>
2016-08-28 01:06:53 -0500

    Menu bar now init by default

--- {{cookiecutter.repo_name}}/{{cookiecutter.package_name}}/tests/test_{{cookiecutter.application_name}}.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.package_name}}/tests/test_{{cookiecutter.application_name}}.py
@@ -25,7 +25,7 @@ def window(qtbot):
     new_window.show()
     return new_window
 
-{% if cookiecutter.insert_menubar == 'yes' %}
+
 @pytest.fixture
 def menu(qtbot):
     """Menu is used as a pytest fixture for tests that need to test the menu bar."""
@@ -33,7 +33,7 @@ def menu(qtbot):
     new_menu_bar = {{cookiecutter.application_name}}.MenuBar()
     qtbot.add_widget(new_menu_bar)
     return new_menu_bar
-{% endif %}
+
 
 def test_window_title(window):
     assert window.windowTitle() == '{{ cookiecutter.application_title }}'
@@ -43,7 +43,7 @@ def test_window_geometry(window):
     assert window.width() == 1024
     assert window.height() == 768
 
-{% if cookiecutter.insert_menubar == 'yes' %}
+
 def test_open_file(window, menu, qtbot, mock):
     """Qtbot clicks on the file sub menu and then navigates to the Open File item. Mock creates
     an object to be passed to the QFileDialog."""
@@ -52,13 +52,3 @@ def test_open_file(window, menu, qtbot, mock):
     qtbot.keyClick(menu.file_sub_menu, Qt.Key_Down)
     mock.patch.object(QFileDialog, 'getOpenFileName', return_value=('', ''))
     qtbot.keyClick(menu.file_sub_menu, Qt.Key_Enter)
-
-
-def test_about_dialog(window, menu, qtbot, mock):
-    """Qtbot clicks on the help sub menu and then navigates to the About item. Mock creates a
-    finished return value so that the QDialog closes."""
-    qtbot.mouseClick(menu.help_sub_menu, Qt.LeftButton)
-    qtbot.keyClick(menu.help_sub_menu, Qt.Key_Down)
-    mock.patch.object(QDialog, 'exec_', return_value='finished')
-    qtbot.keyClick(menu.help_sub_menu, Qt.Key_Enter)
-{% endif %}
--- {{cookiecutter.repo_name}}/{{cookiecutter.package_name}}/{{cookiecutter.application_name}}.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.package_name}}/{{cookiecutter.application_name}}.py
@@ -4,93 +4,6 @@ from PyQt5.QtWidgets import (QAction, QApplication, QDialog, QDesktopWidget, QFi
                              QGroupBox, QHBoxLayout, QLabel, QMainWindow, QMenuBar, QStatusBar,
                              QToolBar)
 
-{% if cookiecutter.insert_menubar == 'yes' %}
-class MenuBar(QMenuBar):
-    """MenuBar class creates a menu bar with necessary items to be used inside of a QMainWindow."""
-
-    def __init__(self, parent=None):
-        """Instantiates the file dialogs and dialog boxes that are used to open files and show
-        messages. Also creates the file menu and help menu."""
-        
-        super(MenuBar, self).__init__(parent)
-
-        self.open_items = OpenDialogs()
-        self.dialogs = Dialogs()
-
-        self.file_menu()
-        self.help_menu()
-
-    def file_menu(self):
-        """Creates a file menu for the menu bar with an Open File item that opens a
-        file dialog."""
-
-        self.file_sub_menu = self.addMenu('File')
-
-        self.open_action = QAction('Open File', self)
-        self.open_action.setStatusTip('Open a file into {{ cookiecutter.application_title }}.')
-        self.open_action.setShortcut('CTRL+O')
-        self.open_action.triggered.connect(self.open_items.open_file)
-
-        self.file_sub_menu.addAction(self.open_action)
-
-    def help_menu(self):
-        """Creates a help menu for the menu bar with a dialog box used to show users helpful
-        information about the application."""
-        self.help_sub_menu = self.addMenu('Help')
-
-        self.about_action = QAction('About', self)
-        self.about_action.setStatusTip('About {{ cookiecutter.application_title }}')
-        self.about_action.setShortcut('CTRL+H')
-        self.about_action.triggered.connect(self.dialogs.about_dialog)
-
-        self.help_sub_menu.addAction(self.about_action)
-
-
-class OpenDialogs(QFileDialog):
-    """The OpenDialogs class stores all activities related to opening files with file dialogs."""
-
-    def __init__(self, parent=None):
-        super(OpenDialogs, self).__init__(parent)
-
-    def open_file(self):
-        """Opens a QFileDialog to allow the user to open a file into the application. The template
-        creates the dialog and simply reads it with the context manager."""
-
-        filename, accepted = QFileDialog.getOpenFileName(self, 'Open File')
-
-        if accepted:
-            with open(filename) as file:
-                file.read()
-
-
-class Dialogs(QDialog):
-    """Because of the wide array of uses of QDialogs, Dialogs is created to store any kind of
-    QDialog item that the user can create."""
-
-    def __init__(self, parent=None):
-        super(Dialogs, self).__init__(parent)
-
-    def about_dialog(self):
-        """Creates a QDialog that shows the user information regarding the application. Uses layouts
-        to arrange the widgets on the dialog."""
-
-        group_box = QGroupBox()
-        layout = QHBoxLayout()
-
-        author = QLabel('Created by {{ cookiecutter.full_name }}', self)
-
-        layout.addWidget(author)
-        group_box.setLayout(layout)
-
-        main_layout = QHBoxLayout()
-        main_layout.addWidget(group_box)
-        main_layout.addStretch(1)
-        self.setLayout(main_layout)
-
-        self.setWindowTitle('About {{ cookiecutter.application_title }}')
-        self.resize(400, 400)
-        self.exec_()
-{% endif %}
 {% if cookiecutter.insert_statusbar == 'yes' %}
 class StatusBar(QToolBar):
     """Creates a status bar to be added to the main window. Inherits QToolBar so that the user
@@ -114,13 +27,42 @@ class {{ cookiecutter.application_title }}(QMainWindow):
         self.resize(1024, 768)
         self.setWindowTitle('{{ cookiecutter.application_title }}')
 
-        {% if cookiecutter.insert_menubar == 'yes' %}
-        self.setMenuBar(MenuBar())
-        {% endif %}
+        self.menu_bar = self.menuBar()
 
         {% if cookiecutter.insert_statusbar == 'yes' %}
-        self.addToolBar(StatusBar())
+        self.status_bar = self.statusBar()
         {% endif %}
+
+        self.file_menu()
+        self.help_menu()
+
+    def file_menu(self):
+        """Creates a file menu for the menu bar with an Open File item that opens a
+        file dialog."""
+
+        self.file_sub_menu = self.menu_bar.addMenu('File')
+
+        self.open_action = QAction('Open File', self)
+        self.open_action.setStatusTip('Open a file into {{ cookiecutter.application_title }}.')
+        self.open_action.setShortcut('CTRL+O')
+        self.open_action.triggered.connect(self.open_items.open_file)
+
+        self.file_sub_menu.addAction(self.open_action)
+
+    def help_menu(self):
+        """Creates a help menu for the menu bar with a dialog box used to show users helpful
+        information about the application."""
+        self.help_sub_menu = self.menu_bar.addMenu('Help')
+
+    def open_file(self):
+        """Opens a QFileDialog to allow the user to open a file into the application. The template
+        creates the dialog and simply reads it with the context manager."""
+
+        filename, accepted = QFileDialog.getOpenFileName(self, 'Open File')
+
+        if accepted:
+            with open(filename) as file:
+                file.read()
 
 def main():
     application = QApplication(sys.argv)
