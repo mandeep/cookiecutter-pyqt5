e4e3cc3ffba2a16013a9e1c7aadc8f67c8ece5df
mandeepbhutani <bhutanimandeep@gmail.com>
2016-09-14 15:14:55 -0500

    Added new test for about dialog

--- {{cookiecutter.repo_name}}/{{cookiecutter.package_name}}/tests/test_{{cookiecutter.application_name}}.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.package_name}}/tests/test_{{cookiecutter.application_name}}.py
@@ -19,7 +19,6 @@ from {{ cookiecutter.package_name }} import {{ cookiecutter.application_name }}
 def window(qtbot):
     """Window is used as a pytest fixture that allows passing it to test functions. Qtbot
     uses qApp to open a new main window."""
-
     new_window = {{cookiecutter.application_name }}.{{cookiecutter.application_title }}()
     qtbot.add_widget(new_window)
     new_window.show()
@@ -38,8 +37,16 @@ def test_window_geometry(window):
 def test_open_file(window, qtbot, mock):
     """Qtbot clicks on the file sub menu and then navigates to the Open File item. Mock creates
     an object to be passed to the QFileDialog."""
-
     qtbot.mouseClick(window.file_sub_menu, Qt.LeftButton)
     qtbot.keyClick(window.file_sub_menu, Qt.Key_Down)
     mock.patch.object(QFileDialog, 'getOpenFileName', return_value=('', ''))
     qtbot.keyClick(window.file_sub_menu, Qt.Key_Enter)
+
+
+def test_about_dialog(window, qtbot, mock):
+    """Qtbot clicks on the help sub menu and then navigates to the About item. Mock creates
+    a QDialog object to be used for the test."""
+    qtbot.mouseClick(window.help_sub_menu, Qt.LeftButton)
+    qtbot.keyClick(window.help_sub_menu, Qt.Key_Down)
+    mock.patch.object(QDialog, 'exec_', return_value='accept')
+    qtbot.keyClick(window.help_sub_menu, Qt.Key_Enter)
