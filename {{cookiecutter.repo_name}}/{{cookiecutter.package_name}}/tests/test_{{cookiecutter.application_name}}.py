98d8789a61efa6acac955120470db22cb390be01
mandeepbhutani <mandeep@users.noreply.github.com>
2016-11-04 13:52:09 -0500

    Removed unused imports, refactored

--- {{cookiecutter.repo_name}}/{{cookiecutter.package_name}}/tests/test_{{cookiecutter.application_name}}.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.package_name}}/tests/test_{{cookiecutter.application_name}}.py
@@ -1,24 +1,14 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-"""
-test_{{ cookiecutter.package_name }}
-----------------------------------
-
-Tests for `{{ cookiecutter.package_name }}` module.
-"""
 import pytest
+
 from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import (QAction, QApplication, QDialog, QDesktopWidget, QFileDialog,
-                             QGroupBox, QHBoxLayout, QLabel, QMainWindow, QMenuBar, QStatusBar,
-                             QToolBar)
+from PyQt5.QtWidgets import QDialog, QFileDialog
+
 from {{ cookiecutter.package_name }} import {{ cookiecutter.application_name }}
 
 
 @pytest.fixture
 def window(qtbot):
-    """Window is used as a pytest fixture that allows passing it to test functions. Qtbot
-    uses qApp to open a new main window."""
+    """Pass the application to the test functions via a pytest fixture."""
     new_window = {{cookiecutter.application_name }}.{{cookiecutter.application_title }}()
     qtbot.add_widget(new_window)
     new_window.show()
@@ -26,17 +16,22 @@ def window(qtbot):
 
 
 def test_window_title(window):
+    """Check that the window title shows as declared."""
     assert window.windowTitle() == '{{ cookiecutter.application_title }}'
 
 
 def test_window_geometry(window):
+    """Check that the window width and height are set as declared."""
     assert window.width() == 1024
     assert window.height() == 768
 
 
 def test_open_file(window, qtbot, mock):
-    """Qtbot clicks on the file sub menu and then navigates to the Open File item. Mock creates
-    an object to be passed to the QFileDialog."""
+    """Test the Open File item of the File submenu.
+
+    Qtbot clicks on the file sub menu and then navigates to the Open File item. Mock creates
+    an object to be passed to the QFileDialog.
+    """
     qtbot.mouseClick(window.file_sub_menu, Qt.LeftButton)
     qtbot.keyClick(window.file_sub_menu, Qt.Key_Down)
     mock.patch.object(QFileDialog, 'getOpenFileName', return_value=('', ''))
@@ -44,8 +39,11 @@ def test_open_file(window, qtbot, mock):
 
 
 def test_about_dialog(window, qtbot, mock):
-    """Qtbot clicks on the help sub menu and then navigates to the About item. Mock creates
-    a QDialog object to be used for the test."""
+    """Test the About item of the Help submenu.
+
+    Qtbot clicks on the help sub menu and then navigates to the About item. Mock creates
+    a QDialog object to be used for the test.
+    """
     qtbot.mouseClick(window.help_sub_menu, Qt.LeftButton)
     qtbot.keyClick(window.help_sub_menu, Qt.Key_Down)
     mock.patch.object(QDialog, 'exec_', return_value='accept')
